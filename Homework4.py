c87ec391543d5cb261129a3075be99ebd2b29b88
SSS-V <167866942+SSS-V@users.noreply.github.com>
2024-04-28 01:16:46 +0300

    Add files via upload

---
+++ Homework4.py
@@ -0,0 +1,7 @@
+immutable_var = (1, True, "work")
+print(immutable_var)
+#Кортеж не изменяемый тип данных. (Элементы кортежа можно изменить,только те которые записаны, [как список]).
+mutable_list = [2, False, "relax"]
+mutable_list[0] = 1
+mutable_list[1] = True
+print(mutable_list)
